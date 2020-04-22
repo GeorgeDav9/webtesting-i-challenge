f6be3bf75e96f6a1ae5766bd98e7ab312ff230a5
George Davila <georgedavila@Georges-MacBook-Pro.local>
2020-04-22 19:43:16 -0400

    finished mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,21 +2,53 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
 };
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+    const successfulItem = {
+      name: item.name,
+      durability: item.durability,
+      enhancement: item.enhancement + 1
+    };
+    return successfulItem;
+  } else {
+    return item;
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    const failedItem = {
+      name: item.name,
+      durability: item.durability - 5,
+      enhancement: item.enhancement
+    };
+    return failedItem;
+  } else if (item.enhancement === 15 || item.enhancement === 16) {
+    const enhanceItemFail = {
+      name: item.name,
+      durability: item.durability - 10,
+      enhancement: item.enhancement
+    };
+    return enhanceItemFail;
+  } else if (item.enhancement > 16) {
+    const greatEnhanceItemFail = {
+      name: item.name,
+      durability: item.durability - 10,
+      enhancement: item.enhancement - 1
+    };
+    return greatEnhanceItemFail;
+  }
+  return item;
 }
 
 function repair(item) {
-  return { ...item };
+  const repairedItem = {
+    name: item.name,
+    durability: 100,
+    enhancement: item.enhancement
+  };
+  return repairedItem;
 }
 
-function get(item) {
-  return { ...item };
-}
